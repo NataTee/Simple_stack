f1d33060189bc0b66d4e14798772fc74aa642471
Natalia Ksenz <velvet@MacBook-Pro.local>
2022-02-11 10:33:06 -0800

    added class Stack

--- stack.h
+++ stack.h
@@ -1,12 +1,32 @@
 /***************************************************************************
 Name: Natalia Ksenz
-Coding 01
+Coding 03
 Purpose: The assignment to further train to build proper architecture, file
 structures, and loosely coupled architectures.
 ***************************************************************************/
-#ifndef COMMANDLINE_STACK_H
-#define COMMANDLINE_STACK_H
+#ifndef STACK_STACK_H
+#define STACK_STACK_H
 
+#include <iostream>
+#define SIZE 10
 
+using std::cout;
+using std::endl;
 
-#endif //COMMANDLINE_STACK_H
+class Stack {
+    public:
+
+    Stack();    // default constructor
+    ~Stack();   //destructor
+
+    void push(int);
+    int pop();
+    void isEmpty();
+
+    protected:
+    //private variables
+    int top;
+    int stack[SIZE];
+};
+
+#endif //STACK_STACK_H
