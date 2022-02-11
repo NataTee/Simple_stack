23ffa22bb6ea85739168e85b2e87b702e5173fee
Natalia Ksenz <velvet@MacBook-Pro.local>
2022-02-11 14:09:08 -0800

    added definition of push function

--- stack.cpp
+++ stack.cpp
@@ -1,7 +1,28 @@
 /***************************************************************************
 Name: Natalia Ksenz
-Coding 01
+Coding 03
 Purpose: The assignment to further train to build proper architecture, file
 structures, and loosely coupled architectures.
 ***************************************************************************/
 #include "stack.h"
+
+Stack::Stack() {
+    top = -1;
+}
+
+Stack::~Stack() {
+}
+
+int Stack::push(int num) {
+
+    bool add = true;
+
+    if (top < SIZE) {
+        stack[++top] = num;
+    }
+    else {
+        add = false;
+    }
+
+    return add;
+}
