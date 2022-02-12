1cc4a2b50e9dc480ad66c383fb285ec020752ab3
Natalia Ksenz <velvet@MacBook-Pro.local>
2022-02-11 22:42:15 -0800

    added for-loops for testing of inserting and deleting numbers

--- main.cpp
+++ main.cpp
@@ -1,6 +1,6 @@
 /***************************************************************************
 Name: Natalia Ksenz
-Coding 01
+Coding 03
 Purpose: The assignment to further train to build proper architecture, file
 structures, and loosely coupled architectures.
 ***************************************************************************/
@@ -8,5 +8,32 @@ structures, and loosely coupled architectures.
 #include "main.h"
 
 int main(int argc, char** argv) {
-    cout << "Hello World" << endl;
+    Stack myStack;
+
+    // Use the time function to get a "seed" value for srand
+    srand((unsigned int)time(0));
+
+    for (int i=0; i<=SIZE+2; i++) {
+        try {
+            int num=rand()%1000;
+            myStack.push(num);
+            cout << num << " inserted" << endl;
+        }
+        catch (const char* msg) {
+            cerr << msg << endl;
+        }
+    }
+
+
+
+    for (int i=0; i<=SIZE+2; i++) {
+        try {
+            cout << myStack.pop() << " removed" << endl;
+        }
+        catch (const char* msg) {
+            cerr << msg << endl;
+        }
+    }
+
+    return 0;
 }
