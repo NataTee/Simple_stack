3f9a2b00550f68384923f029733bdb0a859454a2
Natalia Ksenz <velvet@MacBook-Pro.local>
2022-02-11 23:05:23 -0800

    if-else construction changed to switch

--- main.cpp
+++ main.cpp
@@ -14,24 +14,37 @@ int main(int argc, char** argv) {
     srand((unsigned int)time(0));
 
     for (int test=0; test<=SIZE+100; test++) {
-        int option = rand()%2;
-        if (option==0) {
-            try {
-                int num=rand()%1000;
-                myStack.push(num);
-                cout << "Number " << num << " inserted." << endl;
-            }
-            catch (const char* msg) {
-                cerr << msg << endl;
-            }
-        }
-        else {
-            try {
-                cout << myStack.pop() << " removed." << endl;
-            }
-            catch (const char* msg) {
-                cerr << msg << endl;
-            }
+        int option = rand()%3;
+
+        switch(option) {
+          case 0:
+              try {
+                  int num=rand()%1000;
+                  myStack.push(num);
+                  cout << num << " inserted." << endl;
+              }
+              catch (const char* msg) {
+                  cerr << msg << endl;
+              }
+              break;
+
+          case 1:
+              try {
+                  cout << myStack.pop() << " removed." << endl;
+              }
+              catch (const char* msg) {
+                  cerr << msg << endl;
+              }
+              break;
+
+          case 2:
+              try {
+                  cout << myStack.peek() << " is the last inserted value." << endl;
+              }
+              catch (const char* msg) {
+                  cerr << msg << endl;
+              }
+              break;
         }
     }
 
